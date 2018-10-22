b2a17c38c61a80999ce29cb726cc36f95f7bbe5d
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2018-10-22 09:44:49 -0300

    Add files via upload

---
+++ Negocio/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bologna.Negocio
+{
+    public class Usuario
+    {
+        public static List<Entidades.Usuario> RecuperarTodos()
+        {
+            Datos.Usuario usuarioAdapter = new Datos.Usuario();
+            return usuarioAdapter.RecuperarTodos();
+        }
+
+        public static void Agregar(Entidades.Usuario usuario)
+        {
+            Datos.Usuario usuarioAdapter = new Datos.Usuario();
+            usuarioAdapter.Agregar(usuario);
+        }
+
+        public static bool Validar(string usr, string clave)
+        {
+            Datos.Usuario usuarioAdapter = new Datos.Usuario();
+            return usuarioAdapter.Ingresar(usr,clave);
+        }
+        public static Entidades.Usuario Recuperar(string usr)
+        {
+            Datos.Usuario usuarioAdapter = new Datos.Usuario();
+            return usuarioAdapter.Recuperar(usr);
+        }
+        public static void Eliminar(int usr)
+        {
+            Datos.Usuario usuarioAdapter = new Datos.Usuario();
+            usuarioAdapter.Eliminar(usr);
+        }
+
+    }
+}
